7e2f301e6e917bf30938a20d6cb81b95cba1c725
siruba1 <157345427+siruba1@users.noreply.github.com>
2024-03-29 16:53:45 +0900

    Update se-web-instance.sh

--- se-web-instance.sh
+++ se-web-instance.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo yum install -y httpd
 sudo systemctl enable httpd --now
-sudo echo "<h1>한국 최고!</h1> > /var/www/html/index.html
+sudo echo "<h1>한국 최고!</h1>" > /var/www/html/index.html
